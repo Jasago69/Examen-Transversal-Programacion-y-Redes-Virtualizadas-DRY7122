4cc2b732ee9e24a7d2e1a0be5ddd66d2ed363037
Jasago69 <jo.asena@duocuc.cl>
2023-07-05 08:11:50 +0000

    Agregando archivo Integrantes.py

---
+++ Integrantes.py
@@ -0,0 +1,8 @@
+integrantes = [
+    "Jonathan Alejandro Sebastian Aseña Gomez",
+    "Rodrigo Nicolas Oyarce Rubilar"
+]
+
+# Imprimir la lista de nombres y apellidos
+for integrante in integrantes:
+    print(integrante)
